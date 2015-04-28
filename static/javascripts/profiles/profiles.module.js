962c98a869e181807b20198b3cb5cc5e6eac7653
Christian Witts <cwitts@gmail.com>
2015-04-28 21:51:16 +0200

    Adding initial profiles stubs

---
+++ static/javascripts/profiles/profiles.module.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Chris on 2015/04/28.
+ */
+(function() {
+    'use strict';
+
+    angular
+        .module('thinkster.profiles', [
+            'thinkster.profiles.controllers',
+            'thinkster.profiles.services'
+        ]);
+
+    angular
+        .module('thinkster.profiles.controllers', []);
+
+    angular
+        .module('thinkster.profiles.services', []);
+})();
