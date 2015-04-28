03fd1be46d8d54f90a7cb33d40ff6c241871b59a
Christian Witts <cwitts@gmail.com>
2015-04-28 21:34:25 +0200

    Added singular Post directive, template.

---
+++ static/javascripts/posts/directives/post.directive.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Chris on 2015/04/28.
+ *
+ * Post
+ * @namespace thinkster.posts.directives
+ */
+(function() {
+    'use strict';
+
+    angular
+        .module('thinkster.posts.directives')
+        .directive('post', post);
+
+    /**
+     * @namespace Post
+     */
+    function post() {
+        /**
+         * @name directive
+         * @desc The directive to be returned
+         * @memberOf thinkster.posts.directives.Post
+         */
+        var directive = {
+            restrict: 'E',
+            scope: {
+                post: '='
+            },
+            templateUrl: '/static/templates/posts/post.html'
+        };
+
+        return directive;
+    }
+})();
