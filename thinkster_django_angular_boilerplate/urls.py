d25cd8bb7281a41be57b86f76a004129c2e5f77b
Christian Witts <cwitts@gmail.com>
2015-04-28 17:55:00 +0200

    Updated .gitignore. Added more User stuff

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, url
-
 from thinkster_django_angular_boilerplate.views import IndexView
+from rest_framework_nested import routers
+from authentication.views import AccountViewSet
+
 
 urlpatterns = patterns(
     '',
-
+    url(r'^api/v1/', include(router.urls)),
     url('^.*$', IndexView.as_view(), name='index'),
 )
