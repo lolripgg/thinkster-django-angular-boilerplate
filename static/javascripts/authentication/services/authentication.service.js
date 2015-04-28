17184e7a5901828bf883a66ed05c85ad69ce40b3
Christian Witts <cwitts@gmail.com>
2015-04-28 19:26:14 +0200

    Added more authentication stuff

--- static/javascripts/authentication/services/authentication.service.js
+++ static/javascripts/authentication/services/authentication.service.js
@@ -24,7 +24,12 @@
          * @desc The Factory to be returned
          */
         var Authentication = {
-            register: register
+            getAuthenticatedAccount: getAuthenticatedAccount,
+            isAuthenticated: isAuthenticated,
+            login: login,
+            register: register,
+            setAuthenticatedAccount: setAuthenticatedAccount,
+            unauthenticate: unauthenticate
         };
 
         return Authentication;
@@ -44,6 +49,84 @@
                 password: password,
                 email: email
             });
+        }
+
+        /**
+         * @name login
+         * @desc Try to login with email `email` and password `password`
+         * @param {string} email The email entered by the user
+         * @param {string} password The password entered by the user
+         * @returns {Promise}
+         * @memberOf thinkster.authentication.services.Authentication
+         */
+        function login(email, password) {
+            return $http.post('/api/v1/auth/login/', {
+                email: email,
+                password: password
+            }).then(loginSuccessFn, loginErrorFn);
+
+            /**
+             * @name loginSuccessFn
+             * @desc Set the authenticated account to redirect to index
+             */
+            function loginSuccessFn(data, success, headers, config) {
+                Authentication.setAuthenticatedAccount(data.data);
+
+                window.location = '/';
+            }
+
+            /**
+             * @name loginErrorFn
+             * @desc Log "Epic failure!" to the console
+             */
+            function loginErrorFn(data, status, headers, config) {
+                console.error('Epic failure!');
+            }
+        }
+
+        /**
+         * @name getAuthenticatedAccount
+         * @desc Return te currently authenticated account
+         * @returns {object|undefined} Account if authenticated, else `undefined`
+         * @memberOf thinkster.authentication.services.Authentication
+         */
+        function getAuthenticatedAccount() {
+            if (!$cookies.authenticatedAccount) {
+                return;
+            }
+
+            return JSON.parse($cookies.authenticatedAccount);
+        }
+
+        /**
+         * @name isAuthenticated
+         * @desc Check if the current user is authenticated
+         * @returns {boolean} True if user is authenticated, else false
+         * @memberOf thinkster.authentication.services.Authentication
+         */
+        function isAuthenticated() {
+            return !!$cookies.authenticatedAccount;
+        }
+
+        /**
+         * @name setAuthenticatedAccount
+         * @desc Stringify the account object and store it in a cookie
+         * @param {Object} user The account object to be stored
+         * @returns {undefined}
+         * @memberOf thinkster.authentication.services.Authentication
+         */
+        function setAuthenticatedAccount(account) {
+            $cookies.authenticatedAccount = JSON.stringify(account);
+        }
+
+        /**
+         * @name unauthenticate
+         * @desc Delete the cookie where the user object is stored
+         * @returns {undefined}
+         * @memberOf thinkster.authentication.services.Authentication
+         */
+        function unauthenticate() {
+            delete $cookies.authenticatedAccount;
         }
     }
 })();
--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url, include
 from thinkster_django_angular_boilerplate.views import IndexView
 from rest_framework_nested import routers
-from authentication.views import AccountViewSet
+from authentication.views import AccountViewSet, LoginView
 
 
 router = routers.SimpleRouter()
@@ -9,6 +9,7 @@ router.register(r'accounts', AccountViewSet)
 
 urlpatterns = patterns(
     '',
+    url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
     url(r'^api/v1/', include(router.urls)),
     url('^.*$', IndexView.as_view(), name='index'),
 )
