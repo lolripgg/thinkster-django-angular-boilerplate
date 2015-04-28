2face5a7150da12f19e72a29f12b98816009896c
Christian Witts <cwitts@gmail.com>
2015-04-28 21:01:51 +0200

    Added snackbar support

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -19,5 +19,6 @@ urlpatterns = patterns(
     url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
     url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
     url(r'^api/v1/', include(router.urls)),
+    url(r'^api/v1/', include(accounts_router.urls)),
     url('^.*$', IndexView.as_view(), name='index'),
 )
