89062bc65db2cfe90e696ad33255cfdcc979154e
Christian Witts <cwitts@gmail.com>
2015-04-28 18:01:39 +0200

    Added API endpoint. Added Auth Angular service.

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,8 +1,11 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 from thinkster_django_angular_boilerplate.views import IndexView
 from rest_framework_nested import routers
 from authentication.views import AccountViewSet
 
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
 
 urlpatterns = patterns(
     '',
