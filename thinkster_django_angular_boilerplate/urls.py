49e7f5cb34c6ffb49f42e4d01af7cb4a807f1887
Dinesh Trivedi <dinesh.dart@gmail.com>
2015-09-17 00:24:11 -0300

    Adding login endpoint

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url, include
 from thinkster_django_angular_boilerplate.views import IndexView
 from rest_framework_nested import routers
-from authentication.views import AccountViewSet
+from authentication.views import AccountViewSet, LoginView
 
 router = routers.SimpleRouter()
 router.register(r'accounts', AccountViewSet)
@@ -10,6 +10,7 @@ urlpatterns = patterns(
     '',
     # ... URLs
     url(r'^api/v1/', include(router.urls)),
+    url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
 
     url('^.*$', IndexView.as_view(), name='index'),
 )
