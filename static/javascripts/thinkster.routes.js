4319a9b38eee05221e263e59afbd7c9726842439
Dinesh Trivedi <dinesh.dart@gmail.com>
2015-09-17 00:44:17 -0300

    adding login route

--- static/javascripts/thinkster.routes.js
+++ static/javascripts/thinkster.routes.js
@@ -6,9 +6,13 @@
 
   function config($routeProvider) {
     $routeProvider.when('/register', {
-      controller: 'RegisterController', 
+      controller: 'RegisterController',
       controllerAs: 'vm',
       templateUrl: '/static/templates/authentication/register.html'
+    }).when('/login', {
+      controller: 'LoginController',
+      controllerAs: 'vm',
+      templateUrl: '/static/templates/authentication/login.html'
     }).otherwise('/');
   }
 })();
