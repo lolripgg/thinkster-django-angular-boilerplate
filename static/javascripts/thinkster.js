971bcb6babfd2171b9eb06572dbb089c62d3f404
Dinesh Trivedi <dinesh.dart@gmail.com>
2015-09-17 00:17:23 -0300

    Editing angular dependencies

--- static/javascripts/thinkster.js
+++ static/javascripts/thinkster.js
@@ -1,13 +1,14 @@
 (function () {
   'use strict';
 
+  angular.module('thinkster.config', []);
+  angular.module('thinkster.routes', ['ngRoute']);
+
   angular.module('thinkster', [
     'thinkster.config',
     'thinkster.routes',
     'thinkster.authentication'
   ]);
-
-  angular.module('thinkster.routes', ['ngRoute']);
 
   angular.module('thinkster').run(run);
 
