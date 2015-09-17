ff80293d3ab8d1e3a3ac19f1fa72f686439650a1
Dinesh Trivedi <dinesh.dart@gmail.com>
2015-09-17 00:42:53 -0300

    Creating LoginController

--- static/javascripts/authentication/controllers/register.controller.js
+++ static/javascripts/authentication/controllers/register.controller.js
@@ -8,9 +8,17 @@
     var vm = this;
 
     vm.register = register;
+    activate();
 
     function register() {
       Authentication.register(vm.email, vm.password, vm.username);
+    }
+
+    function activate() {
+      // If the user is authenticated, they should not be here.
+      if (Authentication.isAuthenticated()) {
+        $location.url('/');
+      }
     }
   }
 })();
--- static/javascripts/authentication/services/authentication.service.js
+++ static/javascripts/authentication/services/authentication.service.js
@@ -22,13 +22,31 @@
         username: username,
         password: password,
         email: email
-      });
+      }).then(registerSuccessFn, registerErrorFn);
+
+      function registerSuccessFn(data, status, headers, config) {
+        Authentication.login(email, password);
+      }
+
+      function registerErrorFn(data, status, headers, config) {
+        console.error('Epic failure!');
+      }
     }
 
     function login(email, password) {
       return $http.post('/api/v1/auth/login/', {
         email: email, password: password
-      });
+      }).then(loginSuccessFn, loginErrorFn);
+
+      function loginSuccessFn(data, status, headers, config) {
+        Authentication.setAuthenticatedAccount(data.data);
+
+        window.location = '/';
+      }
+
+      function loginErrorFn(data, status, headers, config) {
+        console.error('Epic failure!');
+      }
     }
 
     function getAuthenticatedAccount() {
