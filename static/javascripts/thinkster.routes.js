d78c985b8b10730ef274b75d50f6a873c41b57dd
Dinesh Trivedi <dinesh.dart@gmail.com>
2015-09-22 23:28:38 -0300

    adding index route

--- static/javascripts/thinkster.routes.js
+++ static/javascripts/thinkster.routes.js
@@ -5,14 +5,22 @@
   config.$inject = ['$routeProvider'];
 
   function config($routeProvider) {
-    $routeProvider.when('/register', {
-      controller: 'RegisterController',
-      controllerAs: 'vm',
-      templateUrl: '/static/templates/authentication/register.html'
-    }).when('/login', {
-      controller: 'LoginController',
-      controllerAs: 'vm',
-      templateUrl: '/static/templates/authentication/login.html'
-    }).otherwise('/');
+    $routeProvider
+      .when('/register', {
+        controller: 'RegisterController',
+        controllerAs: 'vm',
+        templateUrl: '/static/templates/authentication/register.html'
+      })
+      .when('/login', {
+        controller: 'LoginController',
+        controllerAs: 'vm',
+        templateUrl: '/static/templates/authentication/login.html'
+      })
+      .when('/', {
+        controller: 'IndexController',
+        controllerAs: 'vm',
+        templateUrl: '/static/templates/layout/index.html'
+      })
+      .otherwise('/');
   }
 })();
