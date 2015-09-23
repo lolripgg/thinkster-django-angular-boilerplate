b1e210b712abc1735d1db854a369f60a02ff6b13
Dinesh Trivedi <dinesh.dart@gmail.com>
2015-09-23 00:08:06 -0300

    profile settings route, templates and controller

--- static/javascripts/thinkster.routes.js
+++ static/javascripts/thinkster.routes.js
@@ -26,6 +26,11 @@
         controllerAs: 'vm',
         templateUrl: '/static/templates/profiles/profile.html'
       })
+      .when('/+:username/settings', {
+        controller: 'ProfileSettingsController',
+        controllerAs: 'vm',
+        templateUrl: '/static/templates/profiles/settings.html'
+      })
       .otherwise('/');
   }
 })();
